2d2ab105e56ae3b7ee81db6d9b876309437f2cc4
RomainYoobic <rpanaget@yoobic.com>
2020-01-26 18:15:06 +0000

    feat(components): added drawer content component

--- components/DrawerContent.tsx
+++ components/DrawerContent.tsx
@@ -1,22 +1,44 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-navigation";
+import DrawerRow from "./DrawerRow";
+import { gray } from "../shared/colors";
+import IconButton from "./IconButton";
 
 export default function DrawerContent(props) {
   return (
-    <SafeAreaView>
-      <Text onPress={() => props.navigation.navigate("HomeScreen")}>
-        Home Screen
-      </Text>
-      <Text onPress={() => props.navigation.navigate("PokemonListScreen")}>
-        Pokemons Screen
-      </Text>
-      <Text onPress={() => props.navigation.navigate("SearchScreen")}>
-        Search Screen
-      </Text>
-      <Text onPress={() => props.navigation.navigate("LoginScreen")}>
-        Login Screen
-      </Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Image
+          style={{ width: 80, height: 80 }}
+          source={require("../assets/pokeball.png")}
+        />
+      </View>
+      <View style={{ flex: 2, marginHorizontal: 16 }}>
+        <DrawerRow
+          onPress={() => props.navigation.navigate("HomeScreen")}
+          iconName={"ios-home"}
+          title={"Home"}
+        />
+        <DrawerRow
+          onPress={() => props.navigation.navigate("PokemonListScreen")}
+          iconName={"pokeball"}
+          title={"Pokemons"}
+        />
+        <DrawerRow
+          onPress={() => props.navigation.navigate("SearchScreen")}
+          iconName={"ios-search"}
+          title={"Search"}
+        />
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <IconButton
+          onPress={() => props.navigation.navigate("LoginScreen")}
+          name={"ios-power"}
+          size={24}
+          color={gray}
+        />
+      </View>
     </SafeAreaView>
   );
 }
