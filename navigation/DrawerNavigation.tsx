2c1e722842513fee275928c103b07425e3fac24e
RomainYoobic <rpanaget@yoobic.com>
2020-01-26 18:15:53 +0000

    refactor(navigation): added general opening drawer icon

--- navigation/DrawerNavigation.tsx
+++ navigation/DrawerNavigation.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import DrawerContent from "../components/DrawerContent";
@@ -5,28 +6,52 @@ import HomeScreen from "../screens/HomeScreen";
 import PokemonScreen from "../screens/PokemonScreen";
 import PokemonListScreen from "../screens/PokemonListScreen";
 import SearchScreen from "../screens/SearchScreen";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
 
 const Pokemon = {
   screen: PokemonScreen,
   navigationOptions: { title: "Pokemon" }
 };
 
+const headerLeft = navigation => (
+  <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    <Item
+      title={"drawer"}
+      iconName={"ios-menu"}
+      onPress={() => navigation.toggleDrawer()}
+    />
+  </HeaderButtons>
+);
+
 export default createDrawerNavigator(
   {
     HomeScreen: createStackNavigator({
-      HomeScreen: HomeScreen
+      HomeScreen: {
+        screen: HomeScreen,
+        navigationOptions: ({ navigation }) => ({
+          headerTitle: "Home",
+          headerLeft: () => headerLeft(navigation)
+        })
+      }
     }),
     PokemonListScreen: createStackNavigator({
       PokemonListScreen: {
         screen: PokemonListScreen,
-        navigationOptions: { title: "Pokemons" }
+        navigationOptions: ({ navigation }) => ({
+          headerTitle: "Pokemons",
+          headerLeft: () => headerLeft(navigation)
+        })
       },
       PokemonScreen: Pokemon
     }),
     SearchScreen: createStackNavigator({
       SearchScreen: {
         screen: SearchScreen,
-        navigationOptions: { title: "Search" }
+        navigationOptions: ({ navigation }) => ({
+          headerTitle: "Search",
+          headerLeft: () => headerLeft(navigation)
+        })
       },
       PokemonScreen: Pokemon
     })
