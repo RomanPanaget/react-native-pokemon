7c0cfcdc88c6a0bc251472ced2849e94b1dfcf52
RomainYoobic <rpanaget@yoobic.com>
2020-01-26 19:11:06 +0000

    feat(shared): created heartbeat animation

---
+++ shared/animations.ts
@@ -0,0 +1,27 @@
+import { Animated } from "react-native";
+
+export const HeartbeatAnimation = (
+  value: Animated.Value,
+  minValue: number,
+  maxValue: number
+) =>
+  Animated.loop(
+    Animated.sequence([
+      Animated.timing(value, {
+        toValue: maxValue,
+        duration: 100
+      }),
+      Animated.timing(value, {
+        toValue: minValue,
+        duration: 100
+      }),
+      Animated.timing(value, {
+        toValue: maxValue,
+        duration: 100
+      }),
+      Animated.timing(value, {
+        toValue: minValue,
+        duration: 2000
+      })
+    ])
+  );
