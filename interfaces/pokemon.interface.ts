968fdc76b86528d2e071f40f704dc1796a9c405d
RomainYoobic <rpanaget@yoobic.com>
2020-01-27 12:07:59 +0000

    feat(interfaces): added pokemon interface

---
+++ interfaces/pokemon.interface.ts
@@ -0,0 +1,27 @@
+export class Pokemon {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: Array<{
+    slot: number;
+    type: {
+      name: string;
+    };
+  }>;
+  abilities: Array<{
+    ability: {
+      name: string;
+    };
+  }>;
+  sprites: {
+    front_default: string;
+    front_female: string;
+    front_shiny: string;
+    front_shiny_female: string;
+    back_default: string;
+    back_female: string;
+    back_shiny: string;
+    back_shiny_female: string;
+  };
+}
