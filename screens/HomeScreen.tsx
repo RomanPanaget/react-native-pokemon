e37dfff9b7058af2f22e37411d8ed44a904f2e7e
RomainYoobic <rpanaget@yoobic.com>
2020-01-28 00:25:00 +0000

    feat(screens): made home component with heartbeating logo

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,58 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Animated,
+  Linking,
+  Button
+} from "react-native";
+import { HeartbeatAnimation } from "../shared/animations";
+
+const LOGO_MAX_SIZE = 150;
+const LOGO_MIN_SIZE = 145;
 
 export default function HomeScreen() {
-  return <View></View>;
+  const size = useRef(new Animated.Value(LOGO_MIN_SIZE)).current;
+  useEffect(() => {
+    HeartbeatAnimation(size, LOGO_MIN_SIZE, LOGO_MAX_SIZE).start();
+  }, []);
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: LOGO_MAX_SIZE,
+          marginVertical: 24
+        }}
+      >
+        <Animated.Image
+          source={require("../assets/pokeball.png")}
+          style={{ width: size, height: size }}
+        />
+      </View>
+      <View
+        style={{
+          justifyContent: "flex-end",
+          alignItems: "center",
+          marginHorizontal: 24
+        }}
+      >
+        <Text style={{ textAlign: "center" }}>
+          This app is a demo on how to query Pokemon's API with React Native
+        </Text>
+        <Button
+          title={"Github Project"}
+          onPress={() =>
+            Linking.openURL(
+              "https://github.com/RomanPanaget/react-native-pokemon"
+            )
+          }
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
